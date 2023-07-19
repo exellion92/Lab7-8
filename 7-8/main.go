3e9d7b06da1f9679ab6ce95a9ba352de7153b8c4
exellion92 <dima92krem@gmail.com>
2023-07-19 23:17:36 +0300

    done lab

--- 7-8/main.go
+++ 7-8/main.go
@@ -5,40 +5,114 @@ import (
 	"net/http"
 )
 
-type Developer struct {
-	Name         string
-	Organization string
-	Year         int
-	Url          string
+type HeaderData struct {
+	Page string
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/index.html")
-
-	developer := Developer{"Дмитро", "НУВГП", 2023, "https://t.me/NOD_Reaper"}
-
-	tmpl.Execute(w, developer)
+type FooterData struct {
+	URL  string
+	Name string
+	Year int
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/about.html")
-
-	developer := Developer{"Дмитро", "НУВГП", 2023, "https://t.me/NOD_Reaper"}
-
-	tmpl.Execute(w, developer)
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/contact.html")
-
-	developer := Developer{"Дмитро", "НУВГП", 2023, "https://t.me/NOD_Reaper"}
-
-	tmpl.Execute(w, developer)
+type PageData struct {
+	HeaderData HeaderData
+	FooterData FooterData
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/about/", about)
-	http.HandleFunc("/contact/", contact)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about/", aboutHandler)
+	http.HandleFunc("/contact/", contactHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, templateFiles []string, data interface{}) {
+	tmpl, err := template.ParseFiles(templateFiles...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	templateFiles := []string{
+		"templates/home.html",
+		"templates/header.html",
+		"templates/main.html",
+		"templates/footer.html",
+	}
+
+	headerData := HeaderData{
+		Page: "/",
+	}
+
+	footerData := FooterData{
+		URL:  "https://t.me/NOD_Reaper",
+		Name: "Дмитро",
+		Year: 2023,
+	}
+
+	pageData := PageData{
+		HeaderData: headerData,
+		FooterData: footerData,
+	}
+
+	renderTemplate(w, templateFiles, pageData)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	templateFiles := []string{
+		"templates/about.html",
+		"templates/header.html",
+		"templates/footer.html",
+	}
+
+	headerData := HeaderData{
+		Page: "about",
+	}
+
+	footerData := FooterData{
+		URL:  "https://t.me/NOD_Reaper",
+		Name: "Дмитро",
+		Year: 2023,
+	}
+
+	pageData := PageData{
+		HeaderData: headerData,
+		FooterData: footerData,
+	}
+
+	renderTemplate(w, templateFiles, pageData)
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	templateFiles := []string{
+		"templates/contact.html",
+		"templates/header.html",
+		"templates/footer.html",
+	}
+
+	headerData := HeaderData{
+		Page: "contact",
+	}
+
+	footerData := FooterData{
+		URL:  "https://t.me/NOD_Reaper",
+		Name: "Дмитро",
+		Year: 2023,
+	}
+
+	pageData := PageData{
+		HeaderData: headerData,
+		FooterData: footerData,
+	}
+
+	renderTemplate(w, templateFiles, pageData)
 }
